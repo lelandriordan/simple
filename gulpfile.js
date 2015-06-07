50bada0b0bcd7c98083e5d7b783c4f99974acfbf
Leland Riordan <leland@lelandriordan.com>
2015-06-06 20:46:36 -0400

    Added Gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+  watch = require('gulp-watch'),
+  sass = require('gulp-ruby-sass');
+
+var paths = {
+  styles: {
+    src: 'assets/scss/style.scss',
+    dest: './'
+  }
+};
+
+gulp.task('styles', function() {
+  return sass(paths.styles.src)
+  .on('error', function(e) {
+    console.error('Error!', e.message);
+  })
+  .pipe(gulp.dest(paths.styles.dest));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.styles.src, ['styles']);
+});
+
+gulp.task('default', ['watch', 'styles']);
