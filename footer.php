6c71112370921ef0809ef0f1d315029785c0009b
Leland Riordan <leland@lelandriordan.com>
2015-06-08 02:08:57 -0400

    Added footer text

--- footer.php
+++ footer.php
@@ -6,6 +6,8 @@
 </main><!-- .site-main -->
 <footer id="colophon" class="site-footer">
 
+  <p><a href="<?php echo esc_url( __( 'https://github.com/lelandriordan/simple', 'simple' ) ); ?>">Simple</a> by <a href="<?php echo esc_url( __( 'http://www.lelandriordan.com', 'simple' ) ); ?>" rel="designer">Leland Riordan</a></p>
+
 </footer><!-- .site-footer -->
 
 <?php wp_footer(); ?>
